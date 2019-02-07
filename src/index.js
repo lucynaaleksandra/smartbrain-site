7522fa71458b7c3d7034dd636131e12495c806c2
Lucyna Aleksandra <lucyaleksa@gmail.com>
2019-02-07 08:29:08 -0500

    setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import "./index.scss"
 
-console.log("index.js")
+// LAZY LOADING
+document.addEventListener("DOMContentLoaded", function() {
+  var sections = [].slice.call(document.querySelectorAll(".la-animatable"))
 
-function doSomeStupidShitDoes() {
-  var myStupidVar = ["hello", "ok"]
-  var thing = {
-    ok: 1,
-    good: 2,
+  if ("IntersectionObserver" in window) {
+    let sectionObserver = new IntersectionObserver(
+      function(entries, observer) {
+        entries.forEach(function(entry) {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("la-animatable--animate")
+            //sectionObserver.unobserve(entry.target)
+          } else {
+            entry.target.classList.remove("la-animatable--animate")
+          }
+        })
+      },
+      {
+        rootMargin: "0px 0px -500px 0px",
+      }
+    )
+
+    sections.forEach(function(section) {
+      sectionObserver.observe(section)
+    })
   }
-}
+})
